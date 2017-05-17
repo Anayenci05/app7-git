fc3ebc723d6e1ea0484cb1ac74b5c323dc7ec18e
Anayenci05 <anayenci.lopez.flores.cbtis16@gmail.com>
2017-05-17 12:55:03 -0500

    4subida

--- js/acciones.js
+++ js/acciones.js
@@ -4,7 +4,7 @@ $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){
 		
 		$('#izquierda').on("swipeleft",function(){
-	navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion7", "Aceptar"})
+	navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion7","Aceptar"});
 });
 
 		$('#derecha').on("swiperight",function(){
